0dc434cde0aeece10c5e5fc00287db0a25f85887
Muurray <muurimartin@gmail.com>
2026-06-29 11:43:26 +0300

    Update upcoming hike to 1 August 2026 and improve countdown

--- js/sw.js
+++ js/sw.js
@@ -7,12 +7,14 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+
     self.skipWaiting();
 
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES))
     );
+
 });
 
 self.addEventListener("activate", event => {
@@ -28,19 +30,19 @@ self.addEventListener("activate", event => {
     );
 
     self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
 
-    // Never cache the upcoming hikes page
     if (event.request.url.includes("upcoming-hiking-events.html")) {
         event.respondWith(fetch(event.request));
         return;
     }
 
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
+
 });
